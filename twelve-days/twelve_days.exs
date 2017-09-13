4767d44a4c680ea11466641426d3b69b9fdc472e
Raman But-Husaim <raman_but-husaim@live.com>
2017-09-14 00:06:51 +0300

    Twelve days: complete verse method implementation

--- twelve-days/twelve_days.exs
+++ twelve-days/twelve_days.exs
@@ -5,6 +5,7 @@ defmodule TwelveDays do
   """
   @spec verse(number :: integer) :: String.t()
   def verse(number) do
+    get_verse_prefix(number) <> get_gift_postfix(number)
   end
 
   @doc """
@@ -20,6 +21,68 @@ defmodule TwelveDays do
   """
   @spec sing():: String.t()
   def sing do
+  end
+
+  defp get_numeral_for(number) do
+    numeralLookup = %{
+      1 => "first",
+      2 => "second",
+      3 => "third",
+      4 => "fourth",
+      5 => "fifth",
+      6 => "sixth",
+      7 => "seventh",
+      8 => "eighth",
+      9 => "ninth",
+      10 => "tenth",
+      11 => "eleventh",
+      12 => "twelfth"
+    }
+
+    Map.get(numeralLookup, number)
+  end
+
+  defp get_gift_for(number) do
+    giftLookup = %{
+      1 => "a Partridge in a Pear Tree",
+      2 => "two Turtle Doves",
+      3 => "three French Hens",
+      4 => "four Calling Birds",
+      5 => "five Gold Rings",
+      6 => "six Geese-a-Laying",
+      7 => "seven Swans-a-Swimming",
+      8 => "eight Maids-a-Milking",
+      9 => "nine Ladies Dancing",
+      10 => "ten Lords-a-Leaping",
+      11 => "eleven Pipers Piping",
+      12 => "twelve Drummers Drumming"
+    }
+
+    Map.get(giftLookup, number)
+  end
+
+  defp get_verse_prefix(number) do
+    numeral = get_numeral_for(number)
+    "On the #{numeral} day of Christmas my true love gave to me"
+  end
+
+  defp get_gift_postfix(number) do
+    get_gift_postfix("", number)
+  end
+
+  defp get_gift_postfix("", 1) do
+    gift = get_gift_for(1)
+    ", " <> gift <> "."
+  end
+
+  defp get_gift_postfix(postfix, 1) do
+    gift = get_gift_for(1)
+    postfix <> "," <> " and " <> gift <> "."
+  end
+
+  defp get_gift_postfix(postfix, verse) do
+    gift = get_gift_for(verse)
+    get_gift_postfix(postfix <> ", " <> gift, verse - 1)
   end
 end
 
--- twelve-days/twelve_days_test.exs
+++ twelve-days/twelve_days_test.exs
@@ -9,17 +9,15 @@ defmodule TwelveDaysTest do
   use ExUnit.Case
 
   describe "verse" do
-    #@tag :pending
+
     test "first verse" do
       assert TwelveDays.verse(1) == "On the first day of Christmas my true love gave to me, a Partridge in a Pear Tree."
     end
 
-    @tag :pending
     test "sixth verse" do
       assert TwelveDays.verse(6) == "On the sixth day of Christmas my true love gave to me, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
     end
 
-    @tag :pending
     test "last verse" do
       assert TwelveDays.verse(12) == "On the twelfth day of Christmas my true love gave to me, twelve Drummers Drumming, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
     end
