cebf9c3ff028392a83a388eb525ddde9276d4e86
Raman But-Husaim <raman_but-husaim@live.com>
2017-09-14 23:39:17 +0300

    Acronym: completion

--- acronym/acronym.exs
+++ acronym/acronym.exs
@@ -5,5 +5,34 @@ defmodule Acronym do
   """
   @spec abbreviate(String.t()) :: String.t()
   def abbreviate(string) do
+    string
+    |> get_words
+    |> Enum.map(&get_first_letter/1)
+    |> Enum.join
+    |> String.upcase
+  end
+
+  defp get_words(string) do
+    string
+    |> String.replace(~r/([^\w-]|_)+/u, " ")
+    |> String.split
+  end
+
+  def get_first_letter(word) do
+    downcase = String.downcase(word)
+
+    case String.equivalent?(downcase, word) do
+      true -> String.first(word)
+      _ -> word
+          |> String.graphemes
+          |> Enum.filter(&is_uppercase/1)
+          |> Enum.join
+    end
+  end
+
+  defp is_uppercase(grapheme) do
+    uppercase = String.upcase(grapheme)
+
+    String.equivalent?(grapheme, uppercase) && String.match?(grapheme, ~r/\p{L}/)
   end
 end
