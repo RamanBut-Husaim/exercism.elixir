1f14856ce512b72bc3cafa0a8998ec98767888e6
Raman But-Husaim <raman_but-husaim@live.com>
2017-09-14 22:13:39 +0300

    Word count: completion

--- word-count/word_count.exs
+++ word-count/word_count.exs
@@ -6,6 +6,10 @@ defmodule Words do
   """
   @spec count(String.t) :: map
   def count(sentence) do
-
+    sentence
+    |> String.replace(~r/([^\w-]|_)+/u, " ")
+    |> String.downcase
+    |> String.split
+    |> Enum.reduce(%{}, fn (el, acc) -> Map.update(acc, el, 1, &(&1 + 1)) end)
   end
 end
