646c3d1662e9e1ad49c5a3e6f6dc60f00e8b0ee5
Raman But-Husaim <raman_but-husaim@live.com>
2017-09-21 22:29:34 +0300

    Raindrops: completion

--- raindrops/raindrops.exs
+++ raindrops/raindrops.exs
@@ -10,6 +10,50 @@ defmodule Raindrops do
   """
   @spec convert(pos_integer) :: String.t
   def convert(number) do
+    factors = convert([], 1, number)
+    transform_factors("", factors, number)
+  end
 
+  defp convert(factors, factor, number) do
+    case should_continue?(factor) do
+       true ->
+          case rem(number, factor) do
+            0 -> convert([factor | factors], factor + 1, number)
+            _ -> convert(factors, factor + 1, number)
+          end
+        _ -> Enum.reverse(factors)
+    end
+  end
+
+  defp transform_factors("", [], number) do
+    Integer.to_string(number)
+  end
+  defp transform_factors(result, [], _number) do
+    result
+  end
+  defp transform_factors(result, [factor | others], number) do
+    transformed =
+      get_raindrop_factors()
+      |> Map.get(factor, "")
+
+    transform_factors(result <> transformed, others, number)
+  end
+
+  defp should_continue?(factor) do
+    factor <= get_max_factor()
+  end
+
+  defp get_raindrop_factors() do
+    %{
+      3 => "Pling",
+      5 => "Plang",
+      7 => "Plong"
+    }
+  end
+
+  def get_max_factor() do
+    get_raindrop_factors
+    |> Map.keys
+    |> Enum.max
   end
 end
