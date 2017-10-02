ae27ffe44eb9df9a33b058394c0e2d3ba01497e8
Raman But-Husaim <raman_but-husaim@live.com>
2017-10-02 23:32:02 +0300

    Sum Of Multiples: completion

--- sum-of-multiples/sum_of_multiples.exs
+++ sum-of-multiples/sum_of_multiples.exs
@@ -4,6 +4,28 @@ defmodule SumOfMultiples do
   """
   @spec to(non_neg_integer, [non_neg_integer]) :: non_neg_integer
   def to(limit, factors) do
+    calculate(0, limit, 1, factors)
+  end
 
+  defp calculate(sum, limit, current_number, _factors) when current_number >= limit do
+    sum
+  end
+
+  defp calculate(sum, limit, current_number, factors) when current_number < limit do
+    case is_multiple(current_number, factors) do
+      true -> calculate(sum + current_number, limit, current_number + 1, factors)
+      _ -> calculate(sum, limit, current_number + 1, factors)
+    end
+  end
+
+  defp is_multiple(_current_number, []) do
+    false
+  end
+
+  defp is_multiple(current_number, [factor | factors]) do
+    case rem(current_number, factor) === 0 do
+      true -> true
+      _ -> is_multiple(current_number, factors)
+    end
   end
 end
